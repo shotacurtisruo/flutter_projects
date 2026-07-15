3d3aa28822658ea8b19031d668c9fd75af1b6e2e
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2026-07-15 15:45:25 -0700

    main.dart file startup with import and function

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  //custom function 
+  runApp(MyApp()); //functions or executing a function  : this is provided by the flutter framework
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+
