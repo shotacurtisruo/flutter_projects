6b663458782d83c4b940aed5a47238d9819f5557
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2026-07-20 15:25:46 -0700

    more wdigets

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,19 +5,14 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Text("Hello, World!"))),
+        body: Container(
+          child: Center(
+            child: Text("Hello, World!"),
+          )
+        ),
+      ),
     ),
-  ); //functions or executing a function  : this is provided by the flutter framework
-  //^ this runs widgets and builds the widget tree
-  //scaffold is another widget that provides a structure for the app (white bg)
-  //lets add another center widget.
-
-  // eventually make widget trees
-
-  //>formart to organize
-  //enforce styling
+  );
 }
 
 
