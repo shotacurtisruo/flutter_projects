5ab8b6d96ba5eb239e1de5a2738e499060f133ec
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2026-07-20 15:10:10 -0700

    widgets learning

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  //custom function 
-  runApp(const MaterialApp(home: Text("Hello, World!"))); //functions or executing a function  : this is provided by the flutter framework
-   //^ this runs widgets and builds the widget tree
+  //custom function
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Text("Hello, World!"))),
+    ),
+  ); //functions or executing a function  : this is provided by the flutter framework
+  //^ this runs widgets and builds the widget tree
+  //scaffold is another widget that provides a structure for the app (white bg)
+  //lets add another center widget.
+
+  // eventually make widget trees
+
+  //>formart to organize
+  //enforce styling
 }
 
 
